674d4b3d36c3e44e27b77c67b831cfdc26a0c911
vysh1234 <vyshnavivinjam@gmail.com>
2021-10-25 17:54:39 +0530

    Add project files.

---
+++ Models/yourstore.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text.Encodings.Web;
+
+namespace onlineshop.Models
+{
+    public class yourstore : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public string Welcome(string name, int numTimes = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        }
+        public string Onlineorder()
+        {
+            return "All orders Accepted..!";
+        }
+
+    }
+}
